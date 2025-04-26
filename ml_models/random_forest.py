947e78f4441271134664721ba5aade472440ae33
VijayAnand58 <124174294+VijayAnand58@users.noreply.github.com>
2025-04-26 09:46:12 +0530

    random forest with auc score

--- ml_models/random_forest.py
+++ ml_models/random_forest.py
@@ -56,3 +56,27 @@ print(confusion_matrix(y_test, y_pred))
 joblib.dump(model, 'rf_model.pkl')
 print("Model saved successfully!")
 
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+y_prob = model.predict_proba(x_test)[:, 1]  # Probability of class 1
+fpr, tpr, thresholds = roc_curve(y_test, y_prob)
+roc_auc = auc(fpr, tpr)
+
+#plot it
+plt.figure(figsize=(8,6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (AUC = {:.2f})'.format(roc_auc))
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')  # Diagonal line
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC)')
+plt.legend(loc="lower right")
+plt.grid(True)
+plt.show()
+
+# auc Score
+print("AUC score:", roc_auc)
+
+
